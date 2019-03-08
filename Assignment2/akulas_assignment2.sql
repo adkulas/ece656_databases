d1412a47e5106eb4f62744df3cd0d72d10b9e1df
Adam Kulas <adam.kulas1@gmail.com>
2019-03-07 22:16:13 -0500

    questions a and start of b

--- Assignment2/akulas_assignment2.sql
+++ Assignment2/akulas_assignment2.sql
@@ -106,9 +106,103 @@ INSERT INTO Classroom(roomID, Building, Room, Capacity) VALUES('RCH101', 'RCH', 
 INSERT INTO Department(deptID, deptName, faculty) VALUES('ECE', 'Electrical and Computer Engineering', 'Engineering');
 INSERT INTO Department(deptID, deptName, faculty) VALUES('CS', 'Computer Science', 'Math');
 INSERT INTO Department(deptID, deptName, faculty) VALUES('MATH', 'Math', 'Math');
-INSERT INTO Department(deptID, deptName, faculty) VALUES('C&0', 'Combinatorics and Optimization', 'Math');
+INSERT INTO Department(deptID, deptName, faculty) VALUES('C&O', 'Combinatorics and Optimization', 'Math');
 
 
 
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+
+
+
+
+
+-- QUESTION 1
+
+-- (a) What are plausible Primary Keys on each of the five relations?
+
+-- for Instructor, instID could be a valid primary key
+ALTER TABLE Instructor
+ADD PRIMARY KEY (instID);
+
+-- for Course, courseID could be a valid primary key
+ALTER TABLE Course
+ADD PRIMARY KEY (courseID);
+
+-- for Offering couseID,section,termcode could be a valid primary key
+ALTER TABLE Offering
+ADD PRIMARY KEY (courseID,section,termCode);
+
+-- for Classroom roomID could be a valid primary key
+ALTER TABLE Classroom
+ADD PRIMARY KEY (roomID);
+
+-- for Department deptID could be a valid primary key
+ALTER TABLE Department
+ADD PRIMARY KEY (deptID);
+
+-- (b) What are plausible Foreign Keys for the five relations?
+
+ALTER TABLE Instructor
+ADD FOREIGN KEY (deptID) REFERENCES Department(deptID);
+
+
+
+
+
+-- (c) What additional constraints, if any, should be added?
+
+
+/*
+(d) Knowing that each department is part of a faculty (deptID → faculty), that courses can have more than
+one prerequisite, and desiring to be able to do queries based on term (Winter, Spring, Fall) without regard
+to the particular year (e.g., what courses are offered in the fall term?), what modifications to the schema,
+if any, are needed to ensure that it is either 3NF or BCNF (your choice)? If there are any new of changed
+relations, identify them, including any changes or adjustments to primary keys and/or foreign keys, or any
+other constraints. Explain your reasoning.
+*/
+
+
+
+
+
+/*
+(e) Considering queries for the following purposes:
+- Which instructors are sessionals?
+- Which instructors have taught courses over a particular timeframe?
+- How many courses are taught by sessionals?
+- How many students are taught by sessionals?
+- Any of the above, grouped by faculty
+- Any of the above, as fractions of total instructors and/or courses, as relevant?
+
+
+Using “explain” and/or your own reasoning, identify what indexes would be potentially useful to help in
+these queries.
+*/
+
+
+
+/*
+(f) Considering the specific query:
+select count(courseID) from Course inner join Department using (deptID)
+where prereqID is NULL and faculty=’Math’;
+Assuming no indexes, what would the execution plan be and what would be the estimated execution time
+for that plan if the tables are on disk, in contiguous blocks, the number of rows in Course is r c , the number
+of blocks in Course is b c , the number of rows in Department is r d , the number of blocks in Department is
+b d , the time to find a random block on disk is T s and the time to transfer a block from disk is T t ? (g) For
+the query above, identify any indexes over one or more attributes that might potentially improve the query
+performance. For each index you identify, specify the type of index (B+-tree or Hash or either), whether
+or not it is a primary or secondary index, if it is a secondary index identify if it is useful if it is an index
+extension, and justify why the query might benefit from that index.
+*/
+
+
+
+-- QUESTION 2
+-- (2) In assigment 1 you had to compute several queries on the Sean Lahman baseball database. There
+-- were no explicit indexes on that database, though you should have added primary and foreign keys. Using
+-- explain on the queries you created for Lab 1, determine if any additional explicit indexes would help in
+-- solving those queries.
