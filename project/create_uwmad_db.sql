22c86b7d811a4b000644475a24eadda30bd250a5
Adam Kulas <adam.kulas1@gmail.com>
2019-04-22 23:26:37 -0400

    started database create script

---
+++ project/create_uwmad_db.sql
@@ -0,0 +1,190 @@
+CREATE DATABASE IF NOT EXISTS `UWmadison` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `UWmadison`;
+
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- -------------------------------------------------------------
+-- CREATE ALL TABLES
+-- -------------------------------------------------------------
+
+DROP TABLE IF EXISTS `course_offerings`;
+CREATE TABLE `course_offerings` (
+  `uuid` varchar(100) NOT NULL,
+  `course_uuid` varchar(100) NOT NULL,
+  `term_code` decimal(4,0) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE `courses` (
+  `uuid` varchar(100) NOT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `number` int(11) NOT NULL,
+  PRIMARY KEY(`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `grade_distributions`;
+CREATE TABLE `grade_distributions` (
+  `course_offering_uuid` varchar(100) NOT NULL,
+  `section_number` int(11) NOT NULL,
+  `a_count` int(11) NOT NULL,
+  `ab_count` int(11) NOT NULL,
+  `b_count` int(11) NOT NULL,
+  `bc_count` int(11) NOT NULL,
+  `c_count` int(11) NOT NULL,
+  `d_count` int(11) NOT NULL,
+  `f_count` int(11) NOT NULL,
+  `s_count` int(11) NOT NULL,
+  `u_count` int(11) NOT NULL,
+  `cr_count` int(11) NOT NULL,
+  `n_count` int(11) NOT NULL,
+  `p_count` int(11) NOT NULL,
+  `i_count` int(11) NOT NULL,
+  `nw_count` int(11) NOT NULL,
+  `nr_count` int(11) NOT NULL,
+  `other_count` int(11) NOT NULL,
+  PRIMARY KEY(`course_offering_uuid`,`section_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `instructors`;
+CREATE TABLE `instructors` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms` (
+  `uuid` varchar(100) NOT NULL,
+  `facility_code` varchar(30) NOT NULL,
+  `room_code` varchar(10) DEFAULT NULL,
+  PRIMARY KEY(`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `schedules`;
+CREATE TABLE `schedules` (
+  `uuid` varchar(100) NOT NULL,
+  `start_time` int(11) NOT NULL,
+  `end_time` int(11) NOT NULL,
+  `mon` boolean,
+  `tues` boolean,
+  `wed` boolean,
+  `thurs` boolean,
+  `fri` boolean,
+  `sat` boolean,
+  `sun` boolean,
+  PRIMARY KEY(`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `sections`;
+CREATE TABLE `sections` (
+  `uuid` varchar(100) NOT NULL,
+  `course_offering_uuid` varchar(100) NOT NULL,
+  `section_type` varchar(10) NOT NULL,
+  `number` int(11) NOT NULL,
+  `room_uuid` varchar(100) DEFAULT NULL,
+  `schedule_uuid` varchar(100) NOT NULL,
+  PRIMARY KEY(`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `subject_memberships`;
+CREATE TABLE `subject_memberships` (
+  `subject_code` int(11) NOT NULL,
+  `course_offering_uuid` varchar(100) NOT NULL,
+  PRIMARY KEY(`subject_code`,`course_offering_uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `subjects`;
+CREATE TABLE `subjects` (
+  `code` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `abbreviation` varchar(20) NOT NULL,
+  PRIMARY KEY(`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `teachings`;
+CREATE TABLE `teachings` (
+  `instructor_id` int(11) NOT NULL,
+  `section_uuid` varchar(100) NOT NULL,
+  PRIMARY KEY(`instructor_id`,`section_uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+-- -------------------------------------------------------------
+-- LOAD DATA INTO TABLES
+-- -------------------------------------------------------------
+LOAD DATA
+    LOCAL
+    INFILE 'data/course_offerings.csv' REPLACE
+    INTO TABLE course_offerings
+    FIELDS 
+      TERMINATED BY ','
+      ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS
+        (uuid,course_uuid,term_code,@vname)
+        SET 
+        name  = if(@vname='null', NULL, @vname);
+
+
+LOAD DATA
+    LOCAL
+    INFILE 'data/courses.csv' REPLACE
+    INTO TABLE courses
+    FIELDS 
+      TERMINATED BY ','
+      ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS
+        (uuid,@vname,number)
+        SET 
+        name  = if(@vname='null', NULL, @vname);
+
+
+LOAD DATA
+    LOCAL
+    INFILE 'data/grade_distributions.csv' REPLACE
+    INTO TABLE grade_distributions
+    FIELDS 
+      TERMINATED BY ','
+      ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS
+        (course_offering_uuid,section_number,a_count,ab_count,b_count,bc_count,c_count,d_count,f_count,s_count,u_count,cr_count,n_count,p_count,i_count,nw_count,nr_count,other_count);
+
+
+LOAD DATA
+    LOCAL
+    INFILE 'data/rooms.csv' REPLACE
+    INTO TABLE rooms
+    FIELDS 
+      TERMINATED BY ','
+      ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 ROWS
+        (uuid,facility_code,@vroom_code)
+        SET 
+        room_code  = if(@vroom_code='null', NULL, @vroom_code);
+
+
+
+
+
+-- -------------------------------------------------------------
+-- CREATE FOREIGN KEY RELATIONSHIPS AND ADD INDEXES
+-- -------------------------------------------------------------
+
